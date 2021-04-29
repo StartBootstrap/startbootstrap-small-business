88fcd4f78f6badf4ffb2c2c9b865ca6be9e2e348
David Miller <hello@davidmiller.io>
2021-04-28 20:05:11 -0400

    Bootstrap 4.6.0 and dev environment update

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Small Business v4.3.0 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
