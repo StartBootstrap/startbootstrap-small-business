80ef9cb2d5668f0561dd4420813d0e3b483fa978
David Miller <hello@davidmiller.io>
2021-08-05 16:06:11 -0400

    Bootstrap 5.1.0 Update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Small Business v5.0.2 (https://startbootstrap.com/template/small-business)
+* Start Bootstrap - Small Business v5.0.3 (https://startbootstrap.com/template/small-business)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
