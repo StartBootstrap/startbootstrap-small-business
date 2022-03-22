d6970a81532bf29bb9becd1050b0623b22c8f7ba
David Miller <hello@davidmiller.io>
2022-03-22 16:36:49 -0400

    dependencies update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Small Business v5.0.4 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
