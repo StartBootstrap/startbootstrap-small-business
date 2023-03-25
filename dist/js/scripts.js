9c5ef57a25c2c8e1223b274ef7312375925e281c
David Miller <hello@davidmiller.io>
2023-03-25 18:15:21 -0400

    Bootstrap 5.2.3 dependency update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Small Business v5.0.5 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
 */
 // This file is intentionally blank
